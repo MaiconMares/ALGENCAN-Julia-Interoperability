fb5ea71b636f964cb3210d77cb8ddcc6fcc6d2ae
MaiconMares <maiconmaresunb@gmail.com>
2023-04-19 08:03:04 -0300

    Implementing evalhl and composite type.

--- interoperability/julia_interface.jl
+++ interoperability/julia_interface.jl
@@ -1,4 +1,7 @@
 mutable struct mydataptr
+  counters::Vector{Float64}
+
+  mydataptr(counters) = new(zeros(5))
 end
 
 function evalf(n::Int64,x::Vector{Float64},f::Float64,inform::Int64,pdataptr::mydataptr)
@@ -25,8 +28,7 @@ function evalc(n::Int64,x::Vector{Float64},m::Int64,p::Int64,c::Vector{Float64},
 end
 
 function evalj(n::Int64,x::Vector{Float64},m::Int64,p::Int64,ind::Vector{Int32},sorted::Vector{Int32},jsta::Vector{Int64},jlen::Vector{Int64},lim::Int64,jvar::Vector{Int64},jval::Vector{Float64},inform::Int64,pdataptr::mydataptr)
-    nnz1::Int64 = 0
-    nnz2::Int64 = 0
+    # I should define an equivalent call to c_f_pointer(pdataptr,pdata) and an equivalent structure to pdata and pdataptr
 
     if ( ind[1] )
        if ( lim < n )
@@ -39,30 +41,66 @@ function evalj(n::Int64,x::Vector{Float64},m::Int64,p::Int64,ind::Vector{Int32},
        nnz1 = nnz1 + 1
 
        jvar = [i for i in 1:n]
-       jval[1:n] = -1.0
-       nnz2 = nnz2 + n
+       jval[1:n] .= -1.0
 
        sorted[1] = 1
     end
 
     if ( ind[2] )
-       if ( lim .lt. n )
+       if ( lim < n )
           inform = -94
           return
        end
 
-       jsta(nnz1+1) = nnz2 + 1
-       jlen(nnz1+1) = n
+       push!(jsta, length(jval) + 1)
+       push!(jlen, n)
 
-       jvar(nnz2+1:nnz2+n) = (/ (i,i=1,n) /)
+      for i in 1:n
+        push!(jvar, i)
+      end
 
-       jval(nnz2+1) = - 3.0d0 * x[1] ** 2.0d0
-       jval(nnz2+2) = 6.0d0
-       jval(nnz2+3) = 4.0d0
+       push!(jval, - 3.0 * (x[1]^2.0))
+       push!(jval, 6.0)
+       push!(jval, 4.0)
 
        sorted[2] = 1
     end
 end
 
 function evalhl(n::Int64,x::Vector{Float64},m::Int64,p::Int64,lambda::Vector{Float64},lim::Int64,inclf::Int32,hlnnz::Int64,hlrow::Vector{Int64},hlcol::Vector{Int64},hlval::Vector{Float64},inform::Int64,pdataptr::mydataptr)
+  # I should define an equivalent call to c_f_pointer(pdataptr,pdata) and an equivalent structure to pdata and pdataptr
+
+  hlnnz = 0
+
+  # If .not. inclf then the Hessian of the objective function must not be included
+
+  if ( inclf )
+    if ( hlnnz + 2 > lim )
+      inform = -95
+      return
+    end
+
+    hlnnz = 6
+
+    hlrow = [      1,      2,      2,     3,     3,     3 ]
+    hlcol = [      1,      1,      2,     1,     2,     3 ]
+    hlval = [ 10.0, -2.0, 26.0, 2.0, 6.0, 2.0 ]
+
+  end
+
+  # Note that entries of the Hessian of the Lagrangian can be
+  # repeated. If this is case, them sum of repeated entrances is
+  # considered. This feature simplifies the construction of the
+  # Hessian of the Lagrangian.
+
+  if ( hlnnz + 1 > lim )
+    inform = -95
+    return
+  end
+
+  hlnnz = hlnnz + 1
+
+  push!(hlrow, 1)
+  push!(hlcol, 1)
+  push!(hlval, lambda[2] * ( - 6.0 * x[1] ))
 end
