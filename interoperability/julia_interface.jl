b79788bd5a4a59fd965311f1d7ff7d67ba040618
MaiconMares <maiconmaresunb@gmail.com>
2023-04-28 07:54:40 -0300

    Fixing pdataptr argument definition and passing it as optional.

--- interoperability/julia_interface.jl
+++ interoperability/julia_interface.jl
@@ -1,16 +1,16 @@
-mutable struct mydataptr
-  counters::Vector{Float64}
+mutable struct MyDataPtr
+  counters::NTuple{5, Cint}
 
-  mydataptr(counters) = new(zeros(5))
+  MyDataPtr(counters) = new((0,0,0,0,0))
 end
 
-function evalf(n::Int64,x::Vector{Float64},f::Float64,inform::Int64,pdataptr::mydataptr)
+function evalf(n::Int64,x::Vector{Float64},f::Float64,inform::Int64,pdataptr::MyDataPtr=nothing)
     # I should define an equivalent call to c_f_pointer(pdataptr,pdata) and an equivalent structure to pdata and pdataptr
 
     f = ( x[1] + 3.0 * x[2] + x[3] )^(2.0) + 4.0 * (x[1] - x[2])^(2.0)
 end
 
-function evalg(n::Int64,x::Vector{Float64},g::Vector{Float64},inform::Int64,pdataptr::mydataptr)
+function evalg(n::Int64,x::Vector{Float64},g::Vector{Float64},inform::Int64,pdataptr::MyDataPtr=nothing)
     # I should define an equivalent call to c_f_pointer(pdataptr,pdata) and an equivalent structure to pdata and pdataptr
 
     t1::Int64 = x[1] + 3.0 * x[2] + x[3]
@@ -20,14 +20,20 @@ function evalg(n::Int64,x::Vector{Float64},g::Vector{Float64},inform::Int64,pdat
     g[3] = 2.0 * t1
 end
 
-function evalc(n::Int64,x::Vector{Float64},m::Int64,p::Int64,c::Vector{Float64},inform::Int64,pdataptr::mydataptr)
+function evalc(
+  n::Int64,x::Vector{Float64},m::Int64,p::Int64,c::Vector{Float64},inform::Int64,
+  pdataptr::MyDataPtr=nothing
+  )
     # I should define an equivalent call to c_f_pointer(pdataptr,pdata) and an equivalent structure to pdata and pdataptr
 
     c[1] = 1.0 - x[1] - x[2] - x[3]
     c[2] = - 6.0 * x[2] - 4.0 * x[3] + (x[1]^3.0) + 3.0
 end
 
-function evalj(n::Int64,x::Vector{Float64},m::Int64,p::Int64,ind::Vector{Int32},sorted::Vector{Int32},jsta::Vector{Int64},jlen::Vector{Int64},lim::Int64,jvar::Vector{Int64},jval::Vector{Float64},inform::Int64,pdataptr::mydataptr)
+function evalj(n::Int64,x::Vector{Float64},m::Int64,p::Int64,ind::Vector{Int32},
+  sorted::Vector{Int32},jsta::Vector{Int64},jlen::Vector{Int64},lim::Int64,
+  jvar::Vector{Int64},jval::Vector{Float64},inform::Int64,pdataptr::MyDataPtr=nothing
+  )
     # I should define an equivalent call to c_f_pointer(pdataptr,pdata) and an equivalent structure to pdata and pdataptr
 
     if ( ind[1] )
@@ -67,7 +73,11 @@ function evalj(n::Int64,x::Vector{Float64},m::Int64,p::Int64,ind::Vector{Int32},
     end
 end
 
-function evalhl(n::Int64,x::Vector{Float64},m::Int64,p::Int64,lambda::Vector{Float64},lim::Int64,inclf::Int32,hlnnz::Int64,hlrow::Vector{Int64},hlcol::Vector{Int64},hlval::Vector{Float64},inform::Int64,pdataptr::mydataptr)
+function evalhl(
+  n::Int64,x::Vector{Float64},m::Int64,p::Int64,lambda::Vector{Float64},lim::Int64,
+  inclf::Int32,hlnnz::Int64,hlrow::Vector{Int64},hlcol::Vector{Int64},hlval::Vector{Float64},
+  inform::Int64,pdataptr::MyDataPtr=nothing
+  )
   # I should define an equivalent call to c_f_pointer(pdataptr,pdata) and an equivalent structure to pdata and pdataptr
 
   hlnnz = 0
