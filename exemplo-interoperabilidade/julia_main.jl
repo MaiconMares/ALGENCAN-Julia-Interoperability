7b82fdcdf471d70f65b2ae08b38d2826685be1d7
MaiconMares <maiconmaresunb@gmail.com>
2023-06-17 22:52:36 -0300

    Changing ccall deprecated syntax to @ccall macro.

--- exemplo-interoperabilidade/julia_main.jl
+++ exemplo-interoperabilidade/julia_main.jl
@@ -12,12 +12,12 @@ function main()
 
   sum_callback_ptr = @cfunction(sum_callback, Float64, (Ref{Float64}, Ref{Float64}))
 
-  res = ccall((:__fortran_client_MOD_compute_media, "libfortran_client.so"), Float64,
-              (Ptr{Cvoid}, Ref{Float64}, Ref{Float64}), sum_callback_ptr, a, b)
-
+  res = @ccall "./libfortran_client.so".__fortran_client_MOD_compute_media(
+    sum_callback_ptr::Ptr{Cvoid},
+    a::Ref{Float64}, b::Ref{Float64}
+  )::Float64
   # Or just res = ccall(compute_media_, Float64, (Ref{Float64}, Ref{Float64}), a, b)
 
   println(res)
   Libdl.dlclose(lib)
-
 end
